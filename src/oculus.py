4227a4413b7af3a4838467d995f27dc830fca69a
Alex4913 <oneill.alexander.b@gmail.com>
2014-02-22 11:36:34 -0500

    Change of variables for accurate Oculus viewing
    Upon testing the code with the Oculus, a few sign changes needed
    to happen and the format for Euler angles changed from roll-pitch-yaw
    to yaw-pitch-roll

--- src/oculus.py
+++ src/oculus.py
@@ -18,7 +18,7 @@ class Oculus(object):
     self.calibPoint = pyrift.get_orientation()
 
   def getOrientation(self):
-    rpy = map(lambda (x, y):y-x, zip(self.calibPoint, pyrift.get_orientation()))
+    rpy = map(lambda (x,y): y-x, zip(self.calibPoint, pyrift.get_orientation()))
     return rpy
 
   def preGL(self):
@@ -32,22 +32,23 @@ class Oculus(object):
     glClear(GL_DEPTH_BUFFER_BIT)
 
   def draw(self, drawFunc, pos):
-    rpy = self.getOrientation()
+    ypr = self.getOrientation()
 
-    self.leftEye(pos, rpy, drawFunc)
-    self.rightEye(pos, rpy, drawFunc)
+    self.leftEye(pos, ypr, drawFunc)
+    self.rightEye(pos, ypr, drawFunc)
 
-  def leftEye(self, pos, rpy, drawFunc):
+  def leftEye(self, pos, ypr, drawFunc):
     (x, y, z) = pos
-    (roll, pitch, yaw) = rpy
+    (yaw, pitch, roll) = ypr
 
     glPushMatrix()
     glViewport(0, 0, self.width/2, self.height)
     self.preGL()
 
     # Do translations for correct Oculus Viewing
-    glRotatef((pitch * 360) / (2 * math.pi), 1, 0, 0)
-    glRotatef((roll * 360) / (2 * math.pi), 0, 1, 0)
+    glRotatef(-(pitch * 360) / (2 * math.pi), 1, 0, 0)
+    glRotatef(-(yaw * 360) / (2 * math.pi), 0, 1, 0)
+    glRotatef((roll * 360) / (2 * math.pi), 0, 0, 1)
     lookX = math.cos(yaw) + x
     lookY = math.sin(yaw) + y
     offsetX = Oculus.eyeDx * math.cos((math.pi / 2) - yaw)
@@ -59,17 +60,18 @@ class Oculus(object):
     glDisable(GL_SCISSOR_TEST)
     glPopMatrix()
 
-  def rightEye(self, pos, rpy, drawFunc):
+  def rightEye(self, pos, ypr, drawFunc):
     (x, y, z) = pos
-    (roll, pitch, yaw) = rpy
+    (yaw, pitch, roll) = ypr
 
     glPushMatrix()
     glViewport(self.width/2, 0, self.width/2, self.height)
     self.preGL()
 
     # Do translations for correct Oculus Viewing
-    glRotatef((pitch * 360) / (2 * math.pi), 1, 0, 0)
-    glRotatef((roll * 360) / (2 * math.pi), 0, 1, 0)
+    glRotatef(-(pitch * 360) / (2 * math.pi), 1, 0, 0)
+    glRotatef(-(yaw * 360) / (2 * math.pi), 0, 1, 0)
+    glRotatef((roll * 360) / (2 * math.pi), 0, 0, 1)
     lookX = math.cos(yaw) + x
     lookY = math.sin(yaw) + y
     offsetX = -Oculus.eyeDx * math.cos((math.pi / 2) - yaw)
@@ -77,6 +79,5 @@ class Oculus(object):
     gluLookAt(lookX+offsetX, lookY+offsetY, z, x+offsetX, y+offsetY, z, 0, 0, 1)
 
     drawFunc()
-
     glPopMatrix()
 
